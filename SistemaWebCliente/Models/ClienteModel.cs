61f7ff3edfdae27b7a8ce1723a641a4e13dc20ac
jrgventura <jorge.venturag@outlook.com>
2023-08-12 17:33:26 -0500

    Agregar archivos de proyecto.

---
+++ SistemaWebCliente/Models/ClienteModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaWebCliente.Models
+{
+    public class ClienteModel
+    {
+        public int idcliente { get; set; }  
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public int idpais { get; set; }
+        public int telefono { get; set; }
+
+    }
+}
