85c8d03c1bfd53d75d4719c5abd0c37dfdfa53a9
jrgventura <jorge.venturag@outlook.com>
2023-08-12 17:39:46 -0500

    controller

---
+++ SistemaWebCliente/Controllers/NegociosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaWebCliente.Controllers
+{
+    public class NegociosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
