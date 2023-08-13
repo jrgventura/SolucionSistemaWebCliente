d7ca2b97ce27568c90f2b22de31ac0a3f4263c25
jrgventura <jorge.venturag@outlook.com>
2023-08-12 19:04:34 -0500

    funcionalidad buscar y editar

--- SistemaWebCliente/Controllers/NegociosController.cs
+++ SistemaWebCliente/Controllers/NegociosController.cs
@@ -54,5 +54,50 @@ namespace SistemaWebCliente.Controllers
             return View(cliente);
         }
 
+
+        public async Task<ClienteModel> Buscar(int id)
+        {
+            ClienteModel cliente = new ClienteModel();
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress =
+                    new Uri("https://localhost:7274/api/NegocioApi/");
+                HttpResponseMessage response = await
+                    httpClient.GetAsync("GetClientes/"+id);
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                cliente = JsonConvert.DeserializeObject<ClienteModel>(apiResponse);
+
+
+            }
+            return cliente;
+        }
+
+        public async Task<IActionResult> Edit(int id) {
+            return View(await Task.Run(() => Buscar(id)));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ClienteModel cliente)
+        {
+            string mensaje = "";
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress =
+                    new Uri("https://localhost:7274/api/NegocioApi/");
+
+                StringContent content = new StringContent(
+                    JsonConvert.SerializeObject(cliente),
+                    System.Text.Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await
+                    httpClient.PutAsync("UpdateCliente", content);
+
+                mensaje = await response.Content.ReadAsStringAsync();
+            }
+            ViewBag.mensaje = mensaje;
+            return View(cliente);
+        }
+
     }
 }
